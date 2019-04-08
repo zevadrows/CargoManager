7a4747e69b075b96c6e346562405fec9ee0a46d5
Evandro Notebook <zevadrows@gmail.com>
2019-04-08 00:05:00 -0300

    trying first codes to form submit

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 
 //cadastro usuário
 
-var nomeUsuario = document.getElementById("nomeUsuario");
-var sobrenomeUsuario = document.getElementById("sobrenomeUsuario");
-var cpf = document.getElementById("cpf");
-var cnh = document.getElementById("cnh");
-var dataNascimento = document.getElementById("dataNascimento");
-var enderecoUsuario = document.getElementById("enderecoUsuario");
-var numeroEndereco = document.getElementById("numeroEndereco");
-var complementoEndereco = document.getElementById("complementoEndereco");
-var complementoEndereco = document.getElementById("complementoEndereco");
-var emailUsuario = document.getElementById("emailUsuario");
-var celularUsuario = document.getElementById("celularUsuario");
+
+var usuarios = [];
+
+
+
+document.getElementById("btnCadastrarUsuario").addEventListener("click", function () {
+    var usuario = [
+    usuario.nome = document.getElementById("nomeUsuario"),
+    usuario.sobrenome = document.getElementById("sobrenomeUsuario"),
+    usuario.cpf = document.getElementById("cpf"),
+    usuario.cnh = document.getElementById("cnh"),
+    usuario.dataNascimento = document.getElementById("dataNascimento"),
+    usuario.enderecoUsuario = document.getElementById("enderecoUsuario"),
+    usuario.numeroEndereco = document.getElementById("numeroEndereco"),
+    usuario.complementoEndereco = document.getElementById("complementoEndereco"),
+    usuario.emailUsuario = document.getElementById("emailUsuario"),
+    usuario.celularUsuario = document.getElementById("celularUsuario")
+    ]
+    usuarios.push(usuario);
+    localStorage.setItem('usuarios', usuarios);
+
+    console.log(usuarios[0]);
+});
+
+
 
 
