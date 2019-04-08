5e70d6d1d011946de3188ef2d37ce212cbd25252
Evandro Notebook <zevadrows@gmail.com>
2019-04-07 22:44:06 -0300

    add first vars in app file. add submit button in Listagem Agendamento

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//cadastro usuário
+
+var nomeUsuario = document.getElementById("nomeUsuario");
+var sobrenomeUsuario = document.getElementById("sobrenomeUsuario");
+var cpf = document.getElementById("cpf");
+var cnh = document.getElementById("cnh");
+var dataNascimento = document.getElementById("dataNascimento");
+var enderecoUsuario = document.getElementById("enderecoUsuario");
+var numeroEndereco = document.getElementById("numeroEndereco");
+var complementoEndereco = document.getElementById("complementoEndereco");
+var complementoEndereco = document.getElementById("complementoEndereco");
+var emailUsuario = document.getElementById("emailUsuario");
+var celularUsuario = document.getElementById("celularUsuario");
+
+
